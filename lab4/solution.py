b09ca694a04eca6f2105dbf9ba679132e533b6a6
Domin C <dominikcedro00@gmail.com>
2024-12-09 22:03:49 +0100

    UPDATE solution, added burning alrogithm, it needs some fixing because of unexpected tendency

--- lab4/solution.py
+++ lab4/solution.py
@@ -5,13 +5,13 @@ description: Main program for percolation laboratory list of tasks.
 """
 import numpy as np
 import random
-
+import matplotlib.pyplot as plt
 from icecream import ic
 
 # initiate the lattice
 
-SIZE_OF_LATTICE = 10 # from the input file
-site_p = 0.5 # probability of occupation of lattice site
+SIZE_OF_LATTICE = 30 # from the input file
+site_p = 0.8 # probability of occupation of lattice site
 
 def create_lattice(size_of_lattice, site_p):
     """
@@ -35,5 +35,98 @@ def create_lattice(size_of_lattice, site_p):
     print(lattice)
     return lattice
 
-create_lattice(SIZE_OF_LATTICE, site_p)
+lattice = create_lattice(SIZE_OF_LATTICE, site_p)
+##### EXTRA - present lattice
 
+def plot_lattice(lattice):
+    plt.imshow(lattice, interpolation='none')
+    plt.colorbar(label='Site Occupation')
+    plt.title('Lattice Occupation')
+    plt.xlabel('Column')
+    plt.ylabel('Row')
+    plt.show()
+
+# plot_lattice(lattice)
+plot_lattice(lattice)
+
+##### BURNING METHOD
+
+# now i will go over lattice copy
+# for all top occupied i set their value to 2
+# t += 1
+# i go over the top line once again and check for neighbours (if index-1 or +1  == 1 or from above or below if  index equal is ==1
+
+for index, site in enumerate(lattice[0]):
+    if site == 1:
+        lattice[0][index] = 2
+print("")
+# print(lattice)
+
+t_value = 2
+for index, row in enumerate(lattice):
+    change = False
+
+    t_value +=1
+    # ic(t_value)
+    # ic(index, row)
+    if index == 0: # for the first row
+        # only check south
+        row_below = lattice[index+1]
+        # ic(row_below)
+        for site_index, site in enumerate(row):
+            if row_below[site_index] == 1 and site == 2:
+                # ic("below is one for", site, site_index)
+                row_below[site_index] = t_value
+    if index == len(lattice)-1: # for lat row
+        row_above = lattice[index - 1]
+        # ic(row_below)
+        for site_index, site in enumerate(row):
+            # termination statement
+
+            if row_above[site_index] == 1 and site > 1:
+                # ic("below is one for", site, site_index)
+                row_above[site_index] = t_value
+                change = True
+
+            if  site > 1 and row[site_index - 1] == 1:  # check left
+                row[site_index - 1] = t_value
+                change = True
+
+            if site_index != len(row) - 1 and site > 1 and row[site_index + 1] == 1:  # check right
+                row[site_index + 1] = t_value
+                change = True
+
+    if index != len(lattice)-1: # change it to else: later because we are skipping last row
+        row_below = lattice[index + 1]
+        row_above = lattice[index - 1]
+        # ic(row_below)
+        for site_index, site in enumerate(row):
+            # termination statement
+            if row_above[site_index] == 1 and site > 1:
+                # ic("below is one for", site, site_index)
+                row_above[site_index] = t_value
+                change = True
+            if row_below[site_index] == 1 and site > 1:
+                # ic("below is one for", site, site_index)
+                row_below[site_index] = t_value
+                change = True
+
+            if site > 1 and row[site_index-1] == 1: # check left
+                row[site_index - 1] = t_value
+                change = True
+
+            if site_index != len(row)-1 and site > 1 and row[site_index + 1] == 1: # check right
+                row[site_index + 1] = t_value
+                change = True
+        if not change:
+            ic("not changes made", change)
+            break
+
+
+
+plot_lattice(lattice)
+
+# currently I have one issue with the results. because my algorithm searches
+# from left to right there is a tendency to not reach to left side enough
+# im thinking if applying the same algorithm but with other direction would work
+#to fix this issue or maybe applying some regression to left side just to reach far enough?
