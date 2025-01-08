3917fbd4e2aaacadc6bc7a8c9fd574098fdea40d
Domin C <dominikcedro00@gmail.com>
2025-01-08 21:42:20 +0100

    UPDATE for solution in lab4. Cluster sizes calculation and distribution of clusters implemented

--- lab4/solution.py
+++ lab4/solution.py
@@ -94,6 +94,60 @@ def plot_lattice_with_values(lattice):
 
     plt.show()
 
+# HOSHEN - KOPELMAN ALGORITHM
+
+from collections import Counter
+
+def hoshen_kopelman(lattice_raw):
+    k = 2
+    lattice = np.copy(lattice_raw)
+    cluster_sizes = {}
+
+    for row in range(len(lattice)):
+        for column in range(len(lattice[row])):
+            if lattice[row][column] == 1:
+                top = lattice[row-1][column] if row > 0 else 0
+                left = lattice[row][column-1] if column > 0 else 0
+
+                if top == 0 and left == 0:
+                    lattice[row][column] = k
+                    cluster_sizes[k] = 1
+                    k += 1
+                elif top > 0 and left == 0:
+                    lattice[row][column] = top
+                    cluster_sizes[top] += 1
+                elif top == 0 and left > 0:
+                    lattice[row][column] = left
+                    cluster_sizes[left] += 1
+                elif top > 0 and left > 0:
+                    if top == left:
+                        lattice[row][column] = top
+                        cluster_sizes[top] += 1
+                    else:
+                        lattice[row][column] = min(top, left)
+                        cluster_sizes[min(top, left)] += cluster_sizes[max(top, left)] + 1
+                        cluster_sizes[max(top, left)] = -min(top, left)
+
+    for k in range(2, max(cluster_sizes.keys()) + 1):
+        if cluster_sizes[k] > 0:
+            cluster_sizes[k] = sum(1 for v in cluster_sizes.values() if v == k)
+
+    for key in cluster_sizes:
+        cluster_sizes[key] = int(cluster_sizes[key])
+    ic(cluster_sizes)
+    distribution = Counter(cluster_sizes)
+
+    return lattice, cluster_sizes, max(cluster_sizes), distribution
+
+
+
 plot_lattice_with_values(lattice)
 lattice_burning = burning_method(lattice)
-plot_lattice_with_values(lattice_burning)
+# plot_lattice_with_values(lattice_burning)
+lattice_hoshen, cluster_sizes, max_cluster_size, distribution = hoshen_kopelman(lattice)
+# plot_lattice_with_values(lattice_hoshen)
+# print(cluster_sizes)
+ic(cluster_sizes)
+ic(max_cluster_size)
+ic(distribution)
+
